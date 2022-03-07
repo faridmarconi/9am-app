70b04817c8e1f8596ce21ffc9595cbd569f55c0b
Farid <faridmarconi@gmail.com>
2022-03-07 15:31:35 -0300

    quarto comito

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DALEs itero <code>src/App.js</code> and save to reload.
+          DALE <code>src/App.js</code> and save to reload. OK
         </p>
         <a
           className="App-link"
