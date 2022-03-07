e410a830409094c3f0fe2ba7f9f2235036930e95
Farid <faridmarconi@gmail.com>
2022-03-07 15:30:11 -0300

    terceiro comito

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DALE Dalinho <code>src/App.js</code> and save to reload.
+          DALEs itero <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
